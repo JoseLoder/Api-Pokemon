b1fa169c98d9ecdad367b9b24d5c253209999d62
MartinSilvaJose <118887247+MartinSilvaJose@users.noreply.github.com>
2024-05-01 14:17:34 +0200

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import { CardPokemon } from './components/CardPokemon'
 
 function App() {
   const URL = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0';
+
+  const [nextUrl, setNextUrl] = useState(URL);
   const [pokemons, setPokemons] = useState([]);
   const [pokedex, setPokedex] = useState([]);
 
   useEffect(() => {
-    fetch(URL)
+    fetch(nextUrl)
       .then((response) => response.json())
       .then((data) => {
         setPokemons(data.results);
       });
-  }, []);
+  }, [nextUrl]);
 
   useEffect(() => {
     if (pokemons.length === 0) return;
@@ -24,10 +26,22 @@ function App() {
           return { name: item.name, image: data.sprites.front_default };
         });
     });
-    Promise.all(promises).then((newPokemons) => setPokedex(newPokemons));
+    Promise.all(promises)
+      .then((newPokemons) => {
+        if (pokedex.length > 0) {
+          newPokemons = [...pokedex, ...newPokemons];
+        }
+        setPokedex(newPokemons);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pokemons]);
 
-
+  const handleNext = () => {
+    // Extraer el offset=0 de la url para sumarle 10
+    let offset = parseInt(nextUrl.split('offset=')[1]);
+    offset += 10;
+    setNextUrl(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`);
+  }
 
   return (
     <>
@@ -37,6 +51,8 @@ function App() {
           <CardPokemon key={index} pokemon={pokemon} />
         ))}
       </section>
+      <button onClick={handleNext}>Next</button>
+
 
     </>
   )
